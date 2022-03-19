cc022d3f2c8ea36613fb419e3ee036f39b5c6256
Andrey <sn67@inbox.ru>
2022-03-19 15:56:12 +0300

    test

--- lib/test.dart
+++ lib/test.dart
@@ -7,10 +7,49 @@ class Test extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Test'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Test'),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                SizedBox(height: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 180,
+                            color: Color.fromARGB(255, 24, 199, 33),
+                          ),
+                          Container(
+                            alignment: Alignment.bottomRight,
+                            height: 180,
+                            child: Icon(Icons.home),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 180,
+                            color: Color.fromARGB(255, 255, 177, 59),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
